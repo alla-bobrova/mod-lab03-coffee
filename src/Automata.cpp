04a4bd6146ec72c1815e2a8ddd3893e44688e89f
alla-bobrova <75272955+alla-bobrova@users.noreply.github.com>
2023-03-29 22:17:18 +0300

    Update Automata.cpp

--- src/Automata.cpp
+++ src/Automata.cpp
@@ -2,106 +2,88 @@
 #include <iostream>
 
 Automata::Automata() {
-    cash = 0;
     menu = {"Coffee", "Tea", "Cocoa"};
     prices = {50, 30, 40};
+    cash = 0;
     state = OFF;
 }
 
 void Automata::on() {
-    if (state == OFF) {
-        state = WAIT;
-        std::cout << "Automata is on\n";
-    } else {
-        std::cout << "Automata is already on\n";
-    }
+    state = WAIT;
+    std::cout << "The coffee machine is ON\n";
 }
 
 void Automata::off() {
-    if (state == WAIT) {
-        state = OFF;
-        std::cout << "Automata is off\n";
-    } else if (state == ACCEPT) {
-        std::cout << "Can't turn off while accepting money\n";
-    } else if (state == COOK) {
-        std::cout << "Can't turn off while cooking\n";
-    } else {
-        std::cout << "Automata is already off\n";
-    }
+    state = OFF;
+    std::cout << "The coffee machine is OFF\n";
 }
 
 void Automata::coin(int value) {
     if (state == ACCEPT) {
         cash += value;
-        std::cout << "Accepted " << value << " rubles, total: " << cash << " rubles\n";
-    } else {
-        std::cout << "Can't accept coins in current state\n";
+        std::cout << "Inserted " << value << " rubles\n";
+        check();
+    }
+    else {
+        std::cout << "Error: Invalid state\n";
     }
 }
 
-void Automata::getMenu() {
+void Automata::printMenu() {
     std::cout << "Menu:\n";
-    for (int i = 0; i < sizeof(menu) / sizeof(*menu); i++) {
-        std::cout << i+1 << ". " << menu[i] << " - " << prices[i] << " rub\n";
+    for (int i = 0; i < menu.size(); i++) {
+        std::cout << i+1 << ". " << menu[i] << " - " << prices[i] << " rubles\n";
     }
-}
-
-void Automata::getState() {
-    std::string state_str;
-    switch (state) {
-        case OFF:
-            state_str = "OFF";
-            break;
-        case WAIT:
-            state_str = "WAIT";
-            break;
-        case ACCEPT:
-            state_str = "ACCEPT";
-            break;
-        case CHECK:
-            state_str = "CHECK";
-            break;
-        case COOK:
-            state_str = "COOK";
-            break;
-    }
-    std::cout << "Current state: " << state_str << "\n";
 }
 
 void Automata::choice(int item) {
-    if (item >= 1 && item <= sizeof(menu) / sizeof(*menu)) {
+    if (state == WAIT) {
         chosen_item = item;
         state = ACCEPT;
-    } else {
-        std::cout << "Invalid item\n";
+        std::cout << "Chosen item: " << menu[item-1] << "\n";
     }
-}
-
-bool Automata::check() {
-    if (cash >= prices[chosen_item-1]) {
-        state = CHECK;
-        return true;
-    } else {
-        std::cout << "Not enough money\n";
-        return false;
+    else {
+        std::cout << "Error: Invalid state\n";
     }
 }
 
 void Automata::cancel() {
-    if (state == ACCEPT || state == CHECK) {
-        cash = 0;
-        state = WAIT;
+    if (state == ACCEPT) {
         std::cout << "Cancelled\n";
-    } else {std::cout << "Cannot cancel at this time\n";
+        state = WAIT;
+    }
+    else {
+        std::cout << "Error: Invalid state\n";
+    }
 }
+
+void Automata::cook() {
+    if (state == CHECK) {
+        std::cout << "Cooking " << menu[chosen_item-1] << "...\n";
+        state = WAIT;
+    }
+    else {
+        std::cout << "Error: Invalid state\n";
+    }
 }
-void Automata::finish() {
-if (state == COOK) {
-cash = 0;
-state = WAIT;
-std::cout << "Thank you for your purchase!\n";
-} else {
-std::cout << "Cannot finish at this time\n";
+
+void Automata::check() {
+    if (cash >= prices[chosen_item-1]) {
+        std::cout << "Enough money\n";
+        state = CHECK;
+        cash -= prices[chosen_item-1];
+        cook();
+    }
 }
+
+int Automata::getCash() {
+    return cash;
 }
-       
+
+void Automata::setCash(int value) {
+    cash = value;
+}
+
+void Automata::setState(State state) {
+    this->state = state;
+}
