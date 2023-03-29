c3d4b9ab2ad5b698f55c43cbf8be997bab601d20
alla-bobrova <75272955+alla-bobrova@users.noreply.github.com>
2023-03-29 18:53:18 +0300

    Create Automata.h

---
+++ include/Automata.h
@@ -0,0 +1,31 @@
+#ifndef AUTOMATA_H
+#define AUTOMATA_H
+
+#include <string>
+
+enum STATES { OFF, WAIT, ACCEPT, CHECK, COOK };
+
+class Automata {
+public:
+    Automata();
+
+    void on();
+    void off();
+    void coin(int money);
+    void getMenu();
+    STATES getState();
+    void choice(int drink);
+    bool check();
+    void cancel();
+    void cook();
+    void finish();
+
+private:
+    int cash;
+    std::string menu[3] = { "Чай", "Кофе", "Горячий шоколад" };
+    int prices[3] = { 50, 70, 80 };
+    STATES state;
+    int choiceDrink;
+};
+
+#endif  // AUTOMATA_H
