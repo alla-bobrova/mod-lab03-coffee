57e30727947de0bc09bbebb62be276fa8f5746ae
alla-bobrova <75272955+alla-bobrova@users.noreply.github.com>
2023-03-29 22:41:23 +0300

    Update Automata.cpp

--- src/Automata.cpp
+++ src/Automata.cpp
@@ -1,69 +1,48 @@
 #include "Automata.h"
+#include <iostream>
 
 Automata::Automata() {
-    menu = {"Coffee", "Tea", "Cocoa"}; // fixed this line
-    prices = {50, 30, 40}; // fixed this line
-    cash = 0; // fixed this line
-    state = OFF; // fixed these two lines
+    menu = {"Coffee", "Tea", "Cocoa"};
+    prices = {50, 30, 40};
 }
 
 void Automata::on() {
-    state = WAIT;
-    printMenu();
+    std::cout << "Machine is on" << std::endl;
 }
 
 void Automata::off() {
-    cancelTransaction();
-    state = OFF;
+    std::cout << "Machine is off" << std::endl;
 }
 
-void Automata::coin(int n) {
-    if (state == ACCEPT) {
-        cash += n;
-        state = CHECK;
-    }
+void Automata::coin(int coins) {
+    balance += coins;
 }
 
 void Automata::printMenu() {
     for (int i = 0; i < menu.size(); i++) {
         std::cout << i+1 << ". " << menu[i] << " - " << prices[i] << " rubles" << std::endl;
     }
-    state = ACCEPT;
 }
 
-void Automata::choice(int n) {
-    int idx = findMenuIndex(n);
-    if (idx >= 0) {
-        if (cash >= prices[idx]) {
-            cook();
-        } else {
-            state = WAIT;
-        }
+void Automata::select(int item) {
+    if (item >= 1 && item <= menu.size()) {
+        choice = item;
+        std::cout << "Selected item: " << menu[choice-1] << std::endl;
+    } else {
+        std::cout << "Invalid item" << std::endl;
     }
 }
 
 void Automata::cancel() {
-    cancelTransaction();
-    state = WAIT;
+    std::cout << "Canceled" << std::endl;
+    balance = 0;
+    choice = 0;
 }
 
 void Automata::cook() {
-    cash -= prices[findMenuIndex(state)];
-    state = COOK;
+    std::cout << "Cooking..." << std::endl;
 }
 
 void Automata::finish() {
     std::cout << "Enjoy your drink!" << std::endl;
-    cash = 0;
-    state = WAIT;
-}
-
-void Automata::cancelTransaction() {
-    std::cout << "Transaction canceled. Returning " << cash << " rubles." << std::endl;
-    cash = 0;
-    state = WAIT;
-}
-
-int Automata::findMenuIndex(int n) {
-    return n - 1;
 }
