fca46ab21b66ae5bdc9368fcc8369d3932d91cd5
alla-bobrova <75272955+alla-bobrova@users.noreply.github.com>
2023-03-30 12:21:14 +0300

    Update Automata.h

--- include/Automata.h
+++ include/Automata.h
@@ -2,12 +2,7 @@
 #ifndef AUTOMATA_H
 #define AUTOMATA_H
 
-enum STATES {
-    OFF,
-    WAIT,
-    ACCEPT,
-    COOK
-};
+
 
 class Automata {
 public:
@@ -16,20 +11,31 @@ public:
     void off();
     void coin(double money);
     void getMenu();
-    void getState();
+    //void getState();
     void choice(int drink);
     bool check();
     void cancel();
     void cook();
     void finish();
 
+    enum STATES {
+    OFF,
+    WAIT,
+    ACCEPT,
+    COOK
+    };
+    double getCash();
+    int getChosenDrink();
+    STATES getState();
+    STATES state;
+
 private:
     double cash;
     int chosenDrink;
-    STATES state;
     static const int MENU_SIZE = 3;
     std::string menu[MENU_SIZE] = { "Coffee", "Tea", "Milk" };
     double prices[MENU_SIZE] = { 1.5, 1.0, 2.0 };
+
 };
 
 #endif // AUTOMATA_H
