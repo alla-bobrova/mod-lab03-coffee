a3c8cb4d20477f48b720de60af9a4006eedbc604
alla-bobrova <75272955+alla-bobrova@users.noreply.github.com>
2023-03-30 14:22:57 +0300

    Update Automata.h

--- include/Automata.h
+++ include/Automata.h
@@ -2,16 +2,13 @@
 #ifndef AUTOMATA_H
 #define AUTOMATA_H
 
-
-
 class Automata {
-public:
+ public:
     Automata();
     void on();
     void off();
     void coin(double money);
     void getMenu();
-    //void getState();
     void choice(int drink);
     bool check();
     void cancel();
@@ -30,13 +27,12 @@ public:
     STATES getState();
     STATES state;
 
-private:
+ private:
     double cash;
     int chosenDrink;
     static const int MENU_SIZE = 3;
     std::string menu[MENU_SIZE] = { "Coffee", "Tea", "Milk" };
-    double prices[MENU_SIZE] = { 1.5, 1.0, 2.0 };
-
+    double prices[MENU_SIZE] = { 11.5, 10.0, 20.0 };
 };
 
 #endif // AUTOMATA_H
